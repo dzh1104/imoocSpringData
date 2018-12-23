86399557f05388c6d204287f97f2f18602b9896c
dzh1104 <zhding1104@163.com>
2018-12-23 23:23:21 +0800

    学习一些关于spring data的博文

--- src/main/java/com/imooc/repository/EmployeeRepository.java
+++ src/main/java/com/imooc/repository/EmployeeRepository.java
@@ -19,6 +19,25 @@ import org.springframework.data.repository.Repository;
  *  JpaRepository
  *  JpaSpecificationExecutor
  * */
+
+/**
+ * 1）Repository是一个空接口，标记接口，没有包含方法声明的接口
+ *
+ * 2）如果我们定义的接口EmployeeRepository extends Repository
+ *
+ * 如果我们自己的接口没有extends Repository，运行时会报错：
+ * org.springframework.beans.factory.NoSuchBeanDefinitionException: No qualifying bean of type 'org.springdata
+ * .repository.EmployeeRepository' available
+ *
+ * 3) 添加注解能到达到不用extends Repository的功能
+ * @RepositoryDefinition(domainClass = Employee.class, idClass = Integer.class)
+ * */
+
+/*
+* 关于 spring data的博客:
+*   https://www.cnblogs.com/fzng/p/7253149.html
+*   https://www.cnblogs.com/simazilin/p/5645947.html
+* */
 public interface EmployeeRepository extends Repository<Employee, Integer> {
     // 方法名有一定的规则来书写的，才能工作
     public Employee findByName(String name);
--- src/test/java/com/imooc/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/imooc/repository/EmployeeRepositoryTest.java
@@ -4,17 +4,20 @@ import com.imooc.domain.Employee;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-// import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+// import org.junit.jupiter.api.Test;
 
 public class EmployeeRepositoryTest {
 
     private ApplicationContext ctx = null;
+
     private EmployeeRepository employeeRepository = null;
 
     @Before
     public void setup() {
+
         ctx = new ClassPathXmlApplicationContext("beans-new.xml");
         // 从上下文中取出对象 从容器中取出对象
         employeeRepository = ctx.getBean(EmployeeRepository.class);
@@ -23,6 +26,7 @@ public class EmployeeRepositoryTest {
 
     @After
     public void tearDown() {
+
         ctx = null;
         System.out.println("tearDown");
     }
@@ -32,9 +36,7 @@ public class EmployeeRepositoryTest {
 
         System.out.println(employeeRepository);
         Employee employee = employeeRepository.findByName("zhangsan");
-        System.out.println("id:" + employee.getId()
-                + " , name:" + employee.getName()
-                + " ,age:" + employee.getAge());
+        System.out.println("id:" + employee.getId() + " , name:" + employee.getName() + " ,age:" + employee.getAge());
     }
 
 }
